ee16c989dc1217c653760cc07ad2787326d0a1b5
Raphael Torres <110772451+raphavtorres@users.noreply.github.com>
2023-02-06 11:27:37 -0300

    Delete desafio15.py

--- desafio15.py
+++
@@ -1,8 +0,0 @@
-print("######### DESAFIO – 15 ##########")
-
-kilometers = float(input("Kilometers traveled: "))
-days = int(input("Days rented: "))
-
-pay = (days * 60) + (kilometers * 0.15)
-
-print(f"You need to pay R$ {pay:.2f}")
