10d6a8fb2efb393c27fb4d9766210d1b0ddd2866
Raphael Torres <110772451+raphavtorres@users.noreply.github.com>
2023-02-22 13:54:26 -0300

    Update drive_license.py

--- python/exerc_port_python/drive_license.py
+++ python/exerc_port_python/drive_license.py
@@ -1,18 +1,26 @@
-day_current = int(input("Current day: "))
-month_current = int(input("Current month: "))
-year_current = int(input("Current Year: "))
+from datetime import date
+
+day_current = date.today().day
+month_current = date.today().month
+year_current = date.today().year
 
 day_birth = int(input("Day you were born: "))
 month_birth = int(input("Month you were born: "))
 year_birth = int(input("Year you were born: "))
 
-if year_current - year_birth < 18:
-    print("You can't get a driver's license!")
+isValid = True
+
+if year_current - year_birth > 18:
+    pass
 elif month_current < month_birth:
-    print("You can't get a driver's license!")
-elif day_current < day_birth:
-    print("You can't get a driver's license!")
+    isValid = False
+elif month_current == month_birth and day_current < day_birth:
+    isValid = False
 else:
-    print("You can get a drive's license!")
+    pass
 
-
+if isValid:
+    print("You can get a driver's license!")
+else:
+    print("You can't get a driver's license!")
+    
