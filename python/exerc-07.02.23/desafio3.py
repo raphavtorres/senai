d43fadc8fcddaa3066119c3ebec057e242889580
Raphael Torres <110772451+raphavtorres@users.noreply.github.com>
2023-02-24 10:43:49 -0300

    Delete python/exerc-07.02.23 directory

--- python/exerc-07.02.23/desafio3.py
+++
@@ -1,8 +0,0 @@
-print("######### DESAFIO – 03 ##########")
-
-city_name = input("City's name: ").lower()
-city_list = city_name.split()
-
-is_santo = "santo" in city_list[0]
-
-print(f"Stars with 'Santo': {is_santo}")
