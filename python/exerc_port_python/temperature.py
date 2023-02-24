ea31e31516290d62909c51c1b3687eca7ce5336f
Raphael Torres <110772451+raphavtorres@users.noreply.github.com>
2023-02-24 10:43:41 -0300

    Delete python/exerc_port_python directory

--- python/exerc_port_python/temperature.py
+++
@@ -1,6 +0,0 @@
-temp_qnt = input("How many temperatures do you want to insert?: ")
-
-temperatures = []
-for i in temp_qnt:
-    r_temp = input(f"Temperature {i}: ")
-    temperatures.append(r_temp)
