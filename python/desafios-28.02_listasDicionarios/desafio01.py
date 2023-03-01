d312f48590b093670f494a2015baabd7295ebf9e
Raphael Torres <110772451+raphavtorres@users.noreply.github.com>
2023-03-01 07:51:39 -0300

    Update desafio01.py

--- python/desafios-28.02_listasDicionarios/desafio01.py
+++ python/desafios-28.02_listasDicionarios/desafio01.py
@@ -2,13 +2,13 @@ list_num = []
 
 i = 0
 while i < 5:
-   try:
-       num = float(input(f"Digite o {i + 1}º número: "))
-   except Exception:
-       print("VALOR INVÁLIDO! Digite um número!")
-       continue
-   list_num.append(num)
-   i += 1
+    try:
+        num = float(input(f"Digite o {i + 1}º número: "))
+    except ValueError:
+        print("VALOR INVÁLIDO! Digite um número!")
+        continue
+    list_num.append(num)
+    i += 1
 
 list_ord = list_num[:]
 list_ord.sort()
@@ -18,5 +18,8 @@ ult_ind = len(list_num) - 1
 maior_num = list_ord[ult_ind]
 menor_num = list_ord[0]
 
+pos_maior = list_num.index(maior_num)
+pos_menor = list_num.index(menor_num)
+
 print(f"Os números escolhidos foram: {list_num}")
-print(f"O menor número é {menor_num} e o maior é {maior_num}")
+print(f"O menor número é {menor_num} e está na {pos_menor + 1}ª posição . O maior é {maior_num} e está na {pos_maior + 1}ª posição")
