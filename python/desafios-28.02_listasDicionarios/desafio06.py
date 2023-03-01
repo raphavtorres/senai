e48cb57eb9998d866ef04e00645ae1daff972ff4
Raphael Torres <110772451+raphavtorres@users.noreply.github.com>
2023-03-01 20:52:24 -0300

    Update desafio06.py

--- python/desafios-28.02_listasDicionarios/desafio06.py
+++ python/desafios-28.02_listasDicionarios/desafio06.py
@@ -7,7 +7,10 @@ for char in expressao:
 quant_par_esq = lista.count("(")
 quant_par_dir = lista.count(")")
 
-if quant_par_esq != quant_par_dir or lista.index(")") < lista.index("("):
-    print("Expressão inválida")
+if quant_par_dir > 0 or quant_par_esq > 0:
+    if quant_par_esq != quant_par_dir or lista.index(")") < lista.index("("):
+        print("Expressão inválida")
+    else:
+        print("Expressão válida")
 else:
     print("Expressão válida")
