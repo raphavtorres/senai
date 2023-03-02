d8920d49725833013a2a17506aba2bd0421cb352
Raphael Torres <110772451+raphavtorres@users.noreply.github.com>
2023-03-02 10:22:12 -0300

    Update desafio07.py

--- python/desafios-28.02_listasDicionarios/desafio07.py
+++ python/desafios-28.02_listasDicionarios/desafio07.py
@@ -20,11 +20,15 @@ while True:
 menor_peso = min(lista_pesos)
 maior_peso = max(lista_pesos)
 
-pessoa_menor_peso = pessoa[0].index(menor_peso)
-pessoa_maior_peso = pessoa[0].index(maior_peso)
+
+print(menor_peso, maior_peso)
+print(cadastro)
+# pessoa_menor_peso = cadastro.index(menor_peso)
+# pessoa_maior_peso = cadastro.index(menor_peso)
 
 
 print(f"Dados cadastrados: {cadastro}")
 print(f"Total de cadastros: {len(cadastro)}")
-print(f"Maior peso: {pessoa_maior_peso}, com {maior_peso}Kg")
-print(f"Menor peso: {pessoa_menor_peso}, com {menor_peso}")
+# print(f"Maior peso: {pessoa_maior_peso}, com {maior_peso}Kg")
+# print(f"Menor peso: {pessoa_menor_peso}, com {menor_peso}")
+
