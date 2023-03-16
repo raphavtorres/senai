b735b9c1d2e9df893237520e2ed48e3259b02e2a
Raphael Torres <110772451+raphavtorres@users.noreply.github.com>
2023-03-16 16:31:36 -0300

    Add files via upload

--- java/Intermediate_Ex02.java
+++ java/Intermediate_Ex02.java
@@ -1,2 +1,28 @@
+import java.util.Arrays;
+import java.util.Locale;
+import java.util.Random;
+import java.util.Scanner;
+
 public class Intermediate_Ex02 {
+    public static void main(String[] args) {
+        Locale.setDefault(Locale.US);
+        Random random = new Random();
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("How many numbers do you wanna add?: ");
+        int size_array = scanner.nextInt();
+
+        int[] num_array = new int[size_array];
+        int[] inverse_array = new int[size_array];
+
+        int c = size_array - 1;
+        for(int i = 0; i < size_array; i++){
+            num_array[i] = random.nextInt(101);
+            inverse_array[c] = num_array[i];
+            c--;
+        }
+
+        System.out.println(Arrays.toString(num_array));
+        System.out.println(Arrays.toString(inverse_array));
+    }
 }
