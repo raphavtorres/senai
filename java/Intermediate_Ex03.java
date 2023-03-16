be919ac2dbe154bcb496aa6af9f2287929f26039
Raphael Torres <110772451+raphavtorres@users.noreply.github.com>
2023-03-16 16:47:00 -0300

    Add files via upload

---
+++ java/Intermediate_Ex03.java
@@ -0,0 +1,37 @@
+import java.util.Arrays;
+import java.util.Locale;
+import java.util.Random;
+import java.util.Scanner;
+
+public class Intermediate_Ex03 {
+    public static void main(String[] args) {
+        Locale.setDefault(Locale.US);
+        Random random = new Random();
+        Scanner scanner = new Scanner(System.in);
+
+        int size_array, value, nozero_amount = 0;
+
+        System.out.print("How many numbers do you wanna add?: ");
+        size_array = scanner.nextInt();
+
+        int[] num_array = new int[size_array];
+        for(int i = 0; i < size_array; i++){
+            System.out.print("Number: ");
+            value = scanner.nextInt();
+            num_array[i] = value;
+            if (value != 0) {
+                nozero_amount++;
+            }
+        }
+        int[] nozero_array = new int[nozero_amount];
+        int c = 0;
+        for(int i = 0; i < size_array; i++){
+            if (num_array[i] != 0) {
+                nozero_array[c] = num_array[i];
+                c++;
+            }
+        }
+        System.out.println(Arrays.toString(num_array));
+        System.out.println(Arrays.toString(nozero_array));
+    }
+}
