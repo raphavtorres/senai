4b37a84621e8a8e897357aa5f02893e759e1f919
Raphael Torres <110772451+raphavtorres@users.noreply.github.com>
2023-03-21 14:14:51 -0300

    Update teste.py

--- teste.py
+++ teste.py
@@ -12,3 +12,28 @@ interval = range(00)
 
 def falsy(value):
     return 'falsy' if not value else 'truthy'
+
+
+
+
+
+
+# dir, hasattr e getattr
+
+# dir ==> shows all the names inside "string"
+string = "raphael"
+# Debug console
+print(string)
+
+# hasattr ==> checks if a specific object has a specific name inside "string"
+if hasattr(string, 'upper'):
+    print('Existe upper')
+    print(string.upper())
+
+# getattr ==> get a method in a dinamic way, python understands that you're talking about a method
+method = 'lower'
+if hasattr(string, method):
+    print(f'Existe {method}')
+    print(getattr(string, method)())
+else:
+    print(f"Method {method} doesn't exists")
