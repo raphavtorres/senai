6dfb417890caffe3afb9b332b83e6abca2d13e47
Raphael Torres <110772451+raphavtorres@users.noreply.github.com>
2023-03-21 14:01:33 -0300

    Update teste.py

--- teste.py
+++ teste.py
@@ -1,2 +1,14 @@
 # FALSY: (the oposite is truthy)
 list = []
+dictionary = {}
+set = set()
+string = ''
+integer = 0
+float = 0.0
+nothing = None
+boolean = False
+interval = range(00)
+
+
+def falsy(value):
+    return 'falsy' if not value else 'truthy'
