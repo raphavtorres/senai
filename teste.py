42f1597e97ecc87234e1ffdbc133df4b8fa96bd1
Raphael Torres <110772451+raphavtorres@users.noreply.github.com>
2023-03-21 16:59:18 -0300

    Update teste.py

--- teste.py
+++ teste.py
@@ -107,3 +107,22 @@ while True:
         print(next(iterator))
     except StopIteration:
         break
+        
+        
+        
+ #-----------------------------------------------------------------------------------------------------
+# Generator Function uses yield (pauses) instead of return
+
+
+def generator(n=0, max=10):
+    while True:
+        yield n
+        n += 1
+        if n > max:
+            return
+
+
+# gen is an generator object
+gen = generator()
+for i in gen:
+    print(i)
